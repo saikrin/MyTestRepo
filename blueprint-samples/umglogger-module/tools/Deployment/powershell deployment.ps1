a715c27a43858398564b081dcd4411826a9c545c
greg.collingham@umusic.com <greg.collingham@umusic.com>
2014-09-19 16:29:49 +0100

    build test

--- blueprint-samples/umglogger-module/tools/Deployment/powershell deployment.ps1
+++ blueprint-samples/umglogger-module/tools/Deployment/powershell deployment.ps1
@@ -1,7 +1,6 @@
-﻿$env:VersionMinor = $env:UmgEnterpriseServicesBuild_MuleSoftSamples_ApiGateway_BuildApiGateway_build_number
+﻿$env:VersionMinor = %dep.UmgEnterpriseServicesBuild_MuleSoftSamples_ApiGateway_BuildApiGateway.build.counter%
 $env:ServersEnv = "local`$257d1c0c-cb92-48fa-b074-1f1ed6e356e3"
 $env:URL = "http://172.25.172.240:8888/mmc351/api/"
-
 
 #UPLOAD
 $UploadResultJson = .\curl.exe --% --basic -u admin:admin -F file=@..\..\target\umglogger-module-%VersionMinor%-SNAPSHOT.zip -F name=umglogger-module -F version=1.0.%VersionMinor% --header "Content-Type: multipart/form-data" %URL%repository
@@ -10,13 +9,13 @@ $UploadResultObject = $UploadResultJson | ConvertFrom-Json
 $env:UploadedVersionEnv = $UploadResultObject.versionId
 
 #CREATE DEPLOYMENT
-$CreateDeploymentResultJson = curl --% --basic -u admin:admin -d "{\"name\" : \"umglogger-module%VersionMinor%\" ,\"servers\": [ \"%ServersEnv%\" ],\"applications\": [ \"%UploadedVersionEnv%\" ]}" --header "Content-Type: application/json" %URL%deployments
+$CreateDeploymentResultJson = .\curl.exe --% --basic -u admin:admin -d "{\"name\" : \"umglogger-module%VersionMinor%\" ,\"servers\": [ \"%ServersEnv%\" ],\"applications\": [ \"%UploadedVersionEnv%\" ]}" --header "Content-Type: application/json" %URL%deployments
 $CreateDeploymentObject = $CreateDeploymentResultJson | ConvertFrom-Json
 Write-Host $CreateDeploymentResultJson
 Write-Host $CreateDeploymentObject.id
 
 #DEPLOY
 $env:DeploymentIdEnv = $CreateDeploymentObject.id
-$DeployCommand = curl --% --basic -u admin:admin -X POST http://172.25.172.240:8888/mmc351/api/deployments/%DeploymentIdEnv%/deploy
+$DeployCommand = .\curl.exe --% --basic -u admin:admin -X POST http://172.25.172.240:8888/mmc351/api/deployments/%DeploymentIdEnv%/deploy
 Write-Host $DeployCommand
 invoke-expression $DeployCommand
